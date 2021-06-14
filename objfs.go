1f3b8bf265812cc8317313c7dc48b1f9ae964db0
徐佶辉 <xujihui@sean-Home-Mac-16.local>
2021-06-14 21:07:35 +0800

    fix read dir

--- objfs.go
+++ objfs.go
@@ -46,6 +46,11 @@ func NewFS(opt ...Opt) (FS, error) {
 	if bucket, err = client.Bucket(newFsOpts.bucketName); err != nil {
 		return FS{}, fmt.Errorf("failed to find bucket %s, err: %w", newFsOpts.bucketName, err)
 	}
+	acl, err := client.GetBucketACL(newFsOpts.bucketName)
+	if err != nil {
+		return FS{}, fmt.Errorf("failed to get acl of bucket %s", newFsOpts.bucketName)
+	}
+	fmt.Printf("acl %v\n", acl.ACL)
 	return FS{
 		client,
 		bucket,
@@ -92,6 +97,7 @@ func (fileSys FS) ReadDir(name string) ([]fs.DirEntry, error) {
 	return res, nil
 }
 
+//TODO: we may don't need dEntry
 type dEntry struct {
 	prefix string
 	name   string
@@ -114,9 +120,21 @@ func (d dEntry) Info() (fs.FileInfo, error) {
 }
 
 type ObjectFile struct {
-	fs   FS
-	name string
-	r    io.ReadCloser
+	fs     FS
+	name   string
+	isDir  bool
+	offset int //dir offset
+	r      io.ReadCloser
+}
+
+func (file *ObjectFile) ReadDir(n int) ([]fs.DirEntry, error) {
+	//if !file.isDir {
+	//	return nil, &fs.PathError{Op: "readdir", Path: file.name, Err: fs.ErrInvalid}
+	//}
+	//if n > 0 {
+	//	file.fs.bucket.ListObjectsV2(oss.Prefix(file.name), oss.MaxKeys(n))
+	//}
+	return nil, nil
 }
 
 type ObjectFileStat struct {
@@ -127,7 +145,7 @@ type ObjectFileStat struct {
 }
 
 func (o ObjectFileStat) Name() string {
-	return o.name
+	return strings.TrimSuffix(o.name, string(filepath.Separator))
 }
 
 func (o ObjectFileStat) Size() int64 {
@@ -143,7 +161,7 @@ func (o ObjectFileStat) ModTime() time.Time {
 }
 
 func (o ObjectFileStat) IsDir() bool {
-	return false
+	return (o.mode & os.ModeDir) == os.ModeDir
 }
 
 func (o ObjectFileStat) Sys() interface{} {
@@ -165,10 +183,14 @@ func (file *ObjectFile) Stat() (fs.FileInfo, error) {
 	if err != nil {
 		return ObjectFileStat{}, err
 	}
+	mode := os.ModePerm
+	if file.isDir {
+		mode |= os.ModeDir
+	}
 	stat := ObjectFileStat{
 		name:    file.name,
 		size:    int64(size),
-		mode:    0,
+		mode:    mode,
 		modTime: lastModified,
 	}
 	return stat, nil
@@ -182,18 +204,46 @@ func (file *ObjectFile) Close() error {
 	return file.r.Close()
 }
 
+func isNotFoundErr(err error) bool {
+	if e, ok := err.(oss.ServiceError); ok {
+		return e.StatusCode == 404
+	}
+	return false
+}
+
 // Open a/b/c
 func (fileSys FS) Open(name string) (fs.File, error) {
 	if !fs.ValidPath(name) {
 		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrInvalid}
+	}
+	return fileSys.internalOpen(name)
+}
+
+func (fileSys FS) internalOpen(name string) (fs.File, error) {
+	_, err := fileSys.bucket.GetObjectDetailedMeta(name)
+	if err != nil {
+		if isNotFoundErr(err) {
+			name = fmt.Sprintf("%s%s", name, string(filepath.Separator))
+			return fileSys.internalOpen(name)
+		}
+		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrNotExist}
+	}
+	// if name with suffix, then it is directory
+	if strings.HasSuffix(name, "/") {
+		return &ObjectFile{
+			fs:    fileSys,
+			name:  name,
+			isDir: true,
+		}, nil
 	}
 	r, err := fileSys.bucket.GetObject(name)
 	if err != nil {
 		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrNotExist}
 	}
 	return &ObjectFile{
-		fs:   fileSys,
-		name: name,
-		r:    r,
+		fs:    fileSys,
+		name:  name,
+		isDir: false,
+		r:     r,
 	}, nil
 }
